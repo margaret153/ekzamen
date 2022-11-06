0e021ba6c7a8ffd41fcc3780d8c37d72d689f3c1
margaret153 <mrgrtgluhova@gmail.com>
2022-11-06 10:25:27 +0200

    version1.2

--- 11.2022CW/06.11.2022CW.py
+++ 11.2022CW/06.11.2022CW.py
@@ -1,1 +1,36 @@
 class Human:
+    height = 170
+
+class Student(Human):
+    satiety = 0
+
+class Worker(Human):
+    # satiety = 100
+    pass
+
+nick = Student()
+anna = Worker()
+print(nick.height)
+print(anna.height)
+print(nick.satiety)
+print(anna.satiety)
+
+
+class Grandparent:
+    height = 170
+    satiety = 100
+    age = 70
+
+class Parent(Grandparent):
+    age = 45
+
+class Children(Parent):
+    height = 50
+    def __init__(self):
+        print(self.height)
+        print(self.satiety)
+        print(self.age)
+
+nick = Children
+
+
