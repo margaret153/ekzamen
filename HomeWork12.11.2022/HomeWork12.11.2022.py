90d23173b28708dc853675448becdb529f84f885
margaret153 <mrgrtgluhova@gmail.com>
2022-11-12 19:36:56 +0200

    1 version3

--- HomeWork12.11.2022/HomeWork12.11.2022.py
+++ HomeWork12.11.2022/HomeWork12.11.2022.py
@@ -1,34 +1,22 @@
-class Grandparent:
+class Tree Seeds:
     def about(self):
-        print('I am Grandparent')
+        print('I am Tree Seeds')
     def about_myself(self):
-        print('I am Grandparent')
-class Parent(Grandparent):
+        print('I am Tree Seeds')
+class blackbery tree(Tree Seeds):
     def about_myself(self):
-        print('I am Parent')
-class Children(Parent):
-    height = 50
+        print('I am Blackberry Tree')
+class Berry(Blackberry Tree):
+    height = 3
     def __init__(self):
         print(self.height)
         print(self.satiety)
         print(self.age)
 
-class Children(Parent):
+class Blackberry(Berry):
     def __init__(self):
         super().about()
         super().about_myself()
 
-nick = Children()
 
 
-
-class Computer:
-    def __init__(self):
-        self.resolution = "4K"
-class SmartPhone(Computer, Display):
-    def printer(self):
-        print(self.memory)
-        print(self.resolution)
-
-iphone = SmartPhone()
-iphone.printer()
