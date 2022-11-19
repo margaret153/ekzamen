6e8e90f122f970bd5426c5de47c66c9d1b043fc9
margaret153 <mrgrtgluhova@gmail.com>
2022-11-19 18:40:59 +0200

    1.04

--- 13.11.22/work_site.py
+++ 13.11.22/work_site.py
@@ -1,2 +1,8 @@
 import requests
+import fake_useragent
 
+url = 'https://allo.ua/ua/roboty-pylesosy/'
+user = fake_useragent.UserAgent().random
+
+# response = requests.get(url)
+# print(response.text)
