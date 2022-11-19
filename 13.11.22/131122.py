0912df19456fce8f39d927659316c2f6217d8da1
margaret153 <mrgrtgluhova@gmail.com>
2022-11-19 18:42:32 +0200

    1.05

--- 13.11.22/131122.py
+++ 13.11.22/131122.py
@@ -1,9 +1,10 @@
-# my_list = [1,2,3,4,5]
+
+# my_list = [1, 2, 3, 4, 5]
+# # str, list, tuple, set, dict
 # iterator = iter(my_list)
 # print(iterator)
 # print(*iterator)
 
-# print(iterator.__next__())
 # print(iterator.__next__())
 # print(iterator.__next__())
 # print(iterator.__next__())
@@ -23,24 +24,29 @@
 #         self.max_number = max_number
 #
 #     def __iter__(self):
-#         self.i = 0
+#         self.i = -1
 #         return self
 #
 #     def __next__(self):
 #         self.i += 1
-#         if self.i > self.max_number:
+#         if self.i > self.max_number-1:
 #             raise StopIteration
 #         return self.i
 #
 # count = Counter(10)
-# print(*count)
-# for i in count:
-#     print(i)
-#     for i in range(10)
-#         print(i, end=' ')
+# # print(*count)
+# # for i in count:
+# #     print(i)
+# for i in Counter(10):
+#     print(i, end=' ')
+# print()
+# for i in range(10):
+#     print(i, end=' ')
 
 
-# def raise_to_the_degrees(number, max_degrees):
+
+
+# def raise_to_the_degrees(number):
 #     i = 0
 #     while True:
 #         result = number ** i
@@ -50,36 +56,23 @@
 #         i += 1
 #
 #
-# res = raise_to_the_degrees(10, 5)
+# res = raise_to_the_degrees(10)
 # print(res)
 # print(res.__next__())
-# print(res.__next__())
-# print(res.__next__())
-# print(res.__next__())
-# print(res.__next__())
-# print(res.__next__())
-# print(res.__next__())
-# print(res.__next__())
-# for _ in res:
-#     print(_)
 
 # p = print
 # p('Hello')
-#
 # def external():
 #     a = 1
-#
 #     def inner():
 #         print('A =', a)
-#     return inner()
+#     return inner
 #
-# a1 = external
+# a1 = external()
 # a1()
-
 
 # def counter():
 #     count = 0
-#
 #     def inner():
 #         nonlocal count
 #         count +=1
@@ -91,17 +84,32 @@
 # print(a1())
 # b1 = counter()
 # print(b1())
+# print(b1())
+# print(b1())
+# print(b1())
+# print(b1())
+# print(b1())
+# print(b1())
+# print(a1())
 import string
+from random import choice
 def gen_password():
     pas = ''
-
     def inner():
         nonlocal pas
-        pas += (string.ascii_letters)
+        pas += choice(string.ascii_letters)
         return pas
     return inner
 
 password1 = gen_password()
+print(password1())
+print(password1())
+print(password1())
+print(password1())
+print(password1())
+print(password1())
+print(password1())
+
 
 
 
