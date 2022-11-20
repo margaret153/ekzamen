d4adc4abef3425279ea4c7c73f30fad6441c6d27
margaret153 <mrgrtgluhova@gmail.com>
2022-11-20 09:34:18 +0200

    1.4

--- hw19.11.22/19.11.22.py
+++ hw19.11.22/19.11.22.py
@@ -1,39 +1,30 @@
 result = []
 def divider(a, b):
-    if a < b:
-        raise ValueError
-     if b > 100:
-        raise IndexError
+    try:
+        if a < b:
+            result.append(res)
+        if b > 100:
+            result.append(res)
+        if type(a) == "str":
+            result.append(res)
+            if b == 0:
+                raise ZeroDivisionError
+    except IndexError:
+        print('Error index')
+    except ValueError:
+        print('Error value')
+    except TypeError:
+        print('Error type')
+        return
+    except ZeroDivisionError:
+        print('Error ZeroDivisionError')
+        return
     return a/b
 data = {10: 2, 2: 5, "123": 4, 18: 0, []: 15, 8 : 4}
 for key in data:
-    res = divider(key, data[kem])
+    res = divider(key, data[key])
     result.append(res)
 
 print(result)
 
 
-# class Counter:
-#     def __init__(self, max_number):
-#         self.i = 0
-#         self.max_number = max_number
-#
-#     def __iter__(self):
-#         self.i = -1
-#         return self
-#
-#     def __next__(self):
-#         self.i += 1
-#         if self.i > self.max_number-1:
-#             raise StopIteration
-#         return self.i
-#
-# count = Counter(10)
-# # print(*count)
-# # for i in count:
-# #     print(i)
-# for i in Counter(10):
-#     print(i, end=' ')
-# print()
-# for i in range(10):
-#     print(i, end=' ')l
